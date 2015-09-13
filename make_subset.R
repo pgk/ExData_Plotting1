7f628bfa073b61f849b6f6cdb8d4038b9305895a
Panos Kountanis <panosktn@gmail.com>
2015-09-13 18:36:54 +0100

    Update make_subset.R

--- make_subset.R
+++ make_subset.R
@@ -1,4 +1,4 @@
-df = read.table('./household_power_consumption.txt', sep = ';', header = 1)
+df <- read.table('./household_power_consumption.txt', sep = ';', header = 1)
 
 only_dates <- df[df$Date == "1/2/2007" | df$Date == "2/2/2007",]
 write.csv(only_dates, file = "subset.csv")
